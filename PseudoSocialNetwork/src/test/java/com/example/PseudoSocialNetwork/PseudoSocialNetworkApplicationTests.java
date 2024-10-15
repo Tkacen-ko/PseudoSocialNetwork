6252ffdd03717631bd4b22e299356a7f95d9eb11
Tkachenko Dmitrii <dmitry.tkachenko@nordclan.com>
2024-10-15 18:34:36 +0300

    initialization of the project basis

---
+++ PseudoSocialNetwork/src/test/java/com/example/PseudoSocialNetwork/PseudoSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PseudoSocialNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PseudoSocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
